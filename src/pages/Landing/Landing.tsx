d7cb0d1172be8566bb7145c6d0bd113c3d884a17
William Daigle <daiglewilliam13@gmail.com>
2022-04-22 15:21:45 -0700

    update login/landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Landing = () => {
 
     return(
-        <p>Landing Page</p>
+        <div id="landing-wrapper">
+            <div id="login-wrapper">
+                <h3>Log in</h3>
+                <div className="form-wrapper" id="login-form">
+                    <form>
+                        <input placeholder='Email' type="text"></input>
+                        <input placeholder='Password' type="password"></input>
+                        <button type='submit'>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
