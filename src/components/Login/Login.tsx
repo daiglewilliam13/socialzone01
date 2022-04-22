7fc5c7388057078336d28bce7ecd6e0d4196133b
William Daigle <daiglewilliam13@gmail.com>
2022-04-22 15:29:00 -0700

    add copywriting

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const Login = () => {
-
-    return(
-        <div id="login-wrapper">
-            <h3>Log in</h3>
-            <div className="form-wrapper" id="login-form">
-                <form>
-                    <input placeholder='Email' type="text"></input>
-                    <input placeholder='Password' type="password"></input>
-                    <button type='submit'>Login</button>
-                </form>
-            </div>
+const Login = () => (
+    <div id="login-wrapper">
+        <div id="login-text-wrapper">
+            <h3>Welcome to SocialZone!</h3>
+            <p>Come chat, discuss, and showcase yourself on the worlds most active social-network!</p>
         </div>
-    )
-}
+        <h3>Log in</h3>
+        <div className="form-wrapper" id="login-form">
+            <form>
+                <input placeholder='Email' type="text"></input>
+                <input placeholder='Password' type="password"></input>
+                <button type='submit'>Login</button>
+            </form>
+        </div>
+    </div>
+)
 
 export default Login;
