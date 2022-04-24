b3a0e59ab596897f197d8d7d015801e3a5d7ed3e
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 14:37:39 -0700

    update landing page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -7,21 +7,25 @@ const Login = () => (
             <h3>Welcome to SocialZone!</h3>
             <p>Come chat, discuss, and showcase yourself on the worlds most active social-network!</p>
         </div>
-        <h3>Log in</h3>
-        <div className="form-wrapper" id="login-form">
-            <form>
-                <input placeholder='Email' type="text"></input>
-                <input placeholder='Password' type="password"></input>
-                <button type='submit'>Login</button>
-            </form>
+        <div id="login-form">
+            <h3>Log in</h3>
+            <div className="form-wrapper" id="login-form">
+                <form>
+                    <input placeholder='Email' type="text"></input>
+                    <input placeholder='Password' type="password"></input>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+            <hr></hr>
+            <div id="other-auth-wrapper">
+                <p>Or log in with:</p>
+                <button><img src="images/google_login_icon.svg"></img>Google</button>
+                <button><img src="../../images/facebook-login-icon.png"></img>Facebook</button>
+                <div id="signup-wrapper">
+                    <p>New User? <a href="#">Register Here!</a></p>
+                </div>
+            </div>
         </div>
-        <hr></hr>
-        <div id="other-auth-wrapper">
-            <p>Or log in with:</p>
-            <button><img src="images/google_login_icon.svg"></img>Google</button>
-            <button><img src="../../images/facebook-login-icon.png"></img>Facebook</button>
-        </div>
-
     </div>
 )
 
