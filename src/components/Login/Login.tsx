38f3e6553121c15abf398d2ecb157dbe85ab1384
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 15:29:46 -0700

    style login buttons and links

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './login.css';
 import FacebookIcon from '../../images/facebook-login-icon.png';
 const Login = () => (
+    
     <div id="login-wrapper">
         <div id="login-text-wrapper">
             <h3>Welcome to SocialZone!</h3>
@@ -15,19 +16,19 @@ const Login = () => (
                     <input placeholder='email@example.com' type="text" name="email"></input>
                     <label htmlFor="password">Password:</label>
                     <input placeholder='Password' type="password" name="password"></input>
-                    <button type='submit'>Login</button>
+                    <button type='submit' id="login-button">Log in</button>
                 </form>
             </div>
             <hr></hr>
             <div id="other-auth-wrapper">
                 <p>Or log in with:</p>
-                <button><img src="images/google_login_icon.svg"></img>Google</button>
-                <button><img src="../../images/facebook-login-icon.png"></img>Facebook</button>
+                <button id="google-login">Google</button>
+                <button id="facebook-login">Facebook</button>
                 <div id="signup-wrapper">
                     <p>New User? <a href="#">Register Here!</a></p>
                 </div>
             </div>
-        </div>
+        </div>    
     </div>
 )
 
