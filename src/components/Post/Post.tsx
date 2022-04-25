c8922d5cade0bfb6b7ab64c032b1aa567c2edda7
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 10:04:56 -0700

    build and style post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import profilePicture from '../../images/jane-doe.jpg';
-import {BiDownArrow, BiUpArrow} from 'react-icons/bi';
+import { BiDownArrow, BiUpArrow } from 'react-icons/bi';
+import './post.css';
 
 
 const Post = () => {
 
-    return(
+    return (
         <div className="post-wrapper">
             <div className="profile-image">
-                <img src={profilePicture}/>
+                <img src={profilePicture} />
             </div>
-            <div className="content-heading-wrapper">
-                <h3>Jane Doe</h3>
-                <p>on Sat, Jun 11 @ 11:35am</p>
+            <div className="content">
+                <div className="content-heading-wrapper">
+                    <h3>Jane Doe</h3>
+                    <p>on Sat, Jun 11 @ 11:35am</p>
+                </div>
+                <div className="content-body-wrapper">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere tempora culpa rem veniam a voluptatibus dolorem praesentium minima? Omnis nulla odio reprehenderit? Deserunt reiciendis fuga totam, atque molestiae omnis fugiat!</p>
+                </div>
+                <div className="reply-wrapper">
+                    <p>REPLY COMPONENT</p>
+                </div>
+                <div className="engagement-wrapper">
+                    <p>
+                        <BiDownArrow className="vote-icon" />23 : <BiUpArrow className="vote-icon" />2
+                    </p>
+                </div>
             </div>
-            <div className="content-body-wrapper">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere tempora culpa rem veniam a voluptatibus dolorem praesentium minima? Omnis nulla odio reprehenderit? Deserunt reiciendis fuga totam, atque molestiae omnis fugiat!</p>
-            </div>
-            <div className="reply-wrapper">
-                <p>REPLY COMPONENT</p>
-            </div>
-            <div className="engagement-wrapper">
-            <BiDownArrow /><BiUpArrow />
-            </div>    
         </div>
     )
 }
