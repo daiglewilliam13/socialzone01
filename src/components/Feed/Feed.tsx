61ca02d0a04bac6336113c2cd1797d19e3ac0856
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 11:08:59 -0700

    update feed styles

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -4,14 +4,20 @@ import Post from '../Post/Post';
 
 const Feed = () => {
 
-    return(
+    return (
         <>
-        <div id="main-feed-wrapper">
-            <div id="border-wrapper">
-            <h1>Latest Feed</h1><div id="border-line"></div>
+            <div id="main-feed-wrapper">
+                <div id="border-wrapper">
+                    <h1>Latest Feed</h1><div id="border-line"></div>
+                </div>
+                <div className="feed-posts">
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                </div>
             </div>
-        <Post />
-        </div>
         </>
     )
 }
