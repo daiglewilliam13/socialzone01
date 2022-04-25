3a66d3c4e6ddf2a5cf947ca62954e472b431be6b
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 09:48:06 -0700

    build post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,7 +7,19 @@ const Post = () => {
             <div className="profile-image">
                 <img src={profilePicture}/>
             </div>
-            <div className="content-heading-wrapper"></div>
+            <div className="content-heading-wrapper">
+                <h3>Jane Doe</h3>
+                <p>on Sat, Jun 11 @ 11:35am</p>
+            </div>
+            <div className="content-body-wrapper">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere tempora culpa rem veniam a voluptatibus dolorem praesentium minima? Omnis nulla odio reprehenderit? Deserunt reiciendis fuga totam, atque molestiae omnis fugiat!</p>
+            </div>
+            <div className="reply-wrapper">
+                <p>REPLY COMPONENT</p>
+            </div>
+            <div className="engagement-wrapper">
+            
+            </div>    
         </div>
     )
 }
