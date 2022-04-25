7054abf0cdbfd94552bdaec3c4f565e8e0f3f39f
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 19:15:22 -0700

    add createpost component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import TopNav from '../../components/TopNav/TopNav';
 import SideNav from '../../components/SideNav/SideNav';
+import CreatePost from '../../components/CreatePost/CreatePost';
+
 
 const Home = () => {
     return (
         <>
         <TopNav />
         <SideNav />
+        <CreatePost />
         </>
     )
 }
