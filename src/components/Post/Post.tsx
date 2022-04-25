6802d868bd662edd675b2f212e4ef882843de84c
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 10:51:07 -0700

    add collapsing comments section

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import {useState} from 'react';
 import profilePicture from '../../images/jane-doe.jpg';
 import { BiDownArrow, BiUpArrow } from 'react-icons/bi';
 import './post.css';
 import PostComment from '../PostComment/PostComment';
-import { faCommentSlash } from "@fortawesome/free-solid-svg-icons";
-
 const postObject = {
     author: "Jane Doe",
     date: "Sat, Jun 11 @ 11:35am",
@@ -18,11 +17,15 @@ const postObject = {
 
 
 const Post = () => {
-
+    const [expandComments, setExpandComments] = useState(false);
+    const showComments = () => {
+        setExpandComments(expandComments=> !expandComments)
+    }
+    let commentsClassStr = expandComments ? "expanded" : "collapsed";
     return (
         <div className="post-wrapper">
             <div className="profile-image">
-                <img src={profilePicture} />
+                <img alt-text="jane doe" src={profilePicture} />
             </div>
             <div className="content-heading-wrapper">
                 <h2>{postObject.author}</h2>
@@ -40,10 +43,10 @@ const Post = () => {
                     <div className="vote-wrapper">
                         <BiUpArrow className="vote-icon" />{postObject.likes} : <BiDownArrow className="vote-icon" />{postObject.dislikes}
                     </div>
-                    <div className="comments-numbers">{postObject.comments.length} Comments<button className="show-comments-button">(show)</button></div>
+                    <div className="comments-numbers">{postObject.comments.length} Comments<button onClick={showComments}className="show-comments-button">(show)</button></div>
                 </div>
             </div>
-            <div className="comments-wrapper">
+            <div className={`comments-wrapper ${commentsClassStr}`}>
                 {postObject.comments.map((comment: string) => {
                     return (
                         <p>{comment}</p>
