87d149c77cfccb3d62cf295a71ee98ef0d5ab61b
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 19:54:04 -0700

    style create post component

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -5,7 +5,11 @@ const CreatePost = () => {
     
     return(
         <div id="create-post-wrapper">
-            CREATE POST 
+            <p>What's on your mind?</p>
+            <div className="create-post-input-wrapper">
+                <textarea name="" id="" placeholder='Tell us something about your day...'></textarea>
+                <button id="create-post-submit">Post Update</button>
+            </div>
         </div>
     )
 }
