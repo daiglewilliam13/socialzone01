60309f9035862e1dab5604a28a82288f83778091
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 19:01:38 -0700

    update sidebar hook

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './sidenav.css';
+import {useState} from 'react';
+import {FaChevronLeft} from 'react-icons/fa';
 
 
 const SideNav = () => {
-
+    const [expanded, setExpanded] = useState(true);
+    const sidenavStatus = expanded ? 'expanded' : 'collapsed';
+    const moveSideBar = () =>{
+        setExpanded(expanded=> !expanded);
+    }
     return(
-        <div id="sidenav">
-            <div className="side-link-wrapper">
+        <div id="sidenav" className={`sidenav-${sidenavStatus}`}>
+            <div id="side-link-wrapper">
                 <p>Home</p>
                 <p>Friends</p>
                 <p>Messages</p>
@@ -15,6 +21,7 @@ const SideNav = () => {
                 <p>Forums</p>
                 <p>Profile</p>
             </div>
+            <button onClick={moveSideBar} id="sidebar-control"><FaChevronLeft /></button>
         </div>
     )
 }
