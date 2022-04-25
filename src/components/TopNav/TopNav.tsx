e48e7025880616705103a438d06e3a353cef66d9
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 18:07:28 -0700

    add top navbar styles

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './topnav.css';
-import { FaStaylinked } from 'react-icons/fa';
+import { FaStaylinked, FaSearch, FaBell, FaEnvelope, } from 'react-icons/fa';
 const TopNav = () => {
     return(
         <div id="top-navbar-wrapper">
-            <FaStaylinked id="brand-icon"/>
-            <input type="text" id="search-bar"></input>
+            <FaStaylinked className="topnav-icon"/>
+                <input type="text" id="search-bar" placeholder='Search...'></input>
+                <FaSearch className="topnav-icon"/>
+                <FaBell className="topnav-icon" />
+                <FaEnvelope className="topnav-icon" />
+            <img id="topnav-profile-photo" src="../images/john-doe.jpg"></img>
         </div>
     )
 }
