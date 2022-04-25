5a527c468d1c4a3eb7664fc17a6d7539a5293c25
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 14:39:38 -0700

    update grid template

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import SideNav from '../../components/SideNav/SideNav';
 import CreatePost from '../../components/CreatePost/CreatePost';
 import Feed from '../../components/Feed/Feed';
 import FriendList from '../../components/FriendList/FriendList';
-
+import './home.css';
 
 const Home = () => {
     return (
