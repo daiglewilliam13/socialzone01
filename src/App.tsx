c519da06675e7e5bebd10a47ac68e0fe226d94d3
William Daigle <daiglewilliam13@gmail.com>
2022-04-24 17:07:52 -0700

    update react router packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Landing from './pages/Landing/Landing';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <Routes>
+
+      <Route path="/" element={<Landing/>} />
+      </Routes>
     </div>
   );
 }
