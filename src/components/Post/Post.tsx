5c62bbaa9ddc4da99572ddfa811dee7f211b6608
William Daigle <daiglewilliam13@gmail.com>
2022-04-25 10:22:37 -0700

    add postobject for rendering logic

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -9,9 +9,9 @@ const postObject = {
     body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere tempora culpa rem veniam a voluptatibus dolorem praesentium minima? Omnis nulla odio reprehenderit? Deserunt reiciendis fuga totam, atque molestiae omnis fugiat!",
     comments: ["1", "2", "3", "4"],
     likes: 35,
-    likedBy: [12,234,23,123],
+    likedBy: [12, 234, 23, 123],
     dislikes: 3,
-    dislikedBy: [25,2,14]
+    dislikedBy: [25, 2, 14]
 }
 
 
@@ -24,20 +24,20 @@ const Post = () => {
             </div>
             <div className="content">
                 <div className="content-heading-wrapper">
-                    <h2>Jane Doe</h2>
-                    <p>on Sat, Jun 11 @ 11:35am</p>
+                    <h2>{postObject.author}</h2>
+                    <p>on {postObject.date}</p>
                 </div>
                 <div className="content-body-wrapper">
-                    <p></p>
+                    <p>{postObject.body}</p>
                 </div>
                 <div className="reply-wrapper">
                     <p>REPLY COMPONENT</p>
                 </div>
                 <div className="engagement-wrapper">
-                    <p>
-                        <BiDownArrow className="vote-icon" />23 : <BiUpArrow className="vote-icon" />2
-                    </p>
-                    <p>14 Comments <button className="show-comments-button">(show)</button></p>
+                    <div className="vote-wrapper">
+                        <BiUpArrow className="vote-icon" />{postObject.likes} : <BiDownArrow className="vote-icon" />{postObject.dislikes}
+                    </div>
+                    <div className="comments-numbers">{postObject.comments.length} Comments<button className="show-comments-button">(show)</button></div>
                 </div>
             </div>
         </div>
