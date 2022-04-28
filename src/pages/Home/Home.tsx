942b2e199498cdd179b6da5d493ff840e6d85668
William Daigle <daiglewilliam13@gmail.com>
2022-04-28 14:05:36 -0700

    trying to rebuild actions/reducer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Home = () => {
+    const user = useSelector((state: userState)=> state.user)
     return (
         <>
         <TopNav />
