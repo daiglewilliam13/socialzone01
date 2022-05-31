7df831c905c252a9a592c760ad60fe1ff7c39f6f
William Daigle <daiglewilliam13@gmail.com>
2022-05-31 16:23:50 -0700

    update login thunk

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -17,9 +17,7 @@ let initialState = {
     }
 }
 
-export const userLogin = createAsyncThunk(
-    'userLogin',
-    async (email, password) => {
+export const loginUser = createAsyncThunk('userLogin', async (data: {email:string, password:string}) => {
         const response = await fetch('localhost:8080/v1/auth/login', {
             method: 'POST',
             mode: 'cors',
@@ -27,8 +25,8 @@ export const userLogin = createAsyncThunk(
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "email": email,
-                "password": password,
+                "email": data.email,
+                "password": data.password,
             })
         }
         )
@@ -36,14 +34,14 @@ export const userLogin = createAsyncThunk(
     }
 )
 
-export const userReducer = (state = initialState, action: action) => {
-    switch (action.type) {
-        case 'login':
-            return { username: action.payload };
-        default:
-            return state;
-    }
-}
+// export const userReducer = (state = initialState, action: action) => {
+//     switch (action.type) {
+//         case 'login':
+//             return { username: action.payload };
+//         default:
+//             return state;
+//     }
+// }
 
 const userSlice = createSlice({
     name: 'user',
@@ -52,14 +50,14 @@ const userSlice = createSlice({
     },
     extraReducers: builder =>{
         builder
-        .addCase(userLogin.pending, (state, action)=>{
+        .addCase(loginUser.pending, (state, action)=>{
             state.status.message = "logging in";
         })
-        .addCase(userLogin.fulfilled, (state, action)=>{
+        .addCase(loginUser.fulfilled, (state, action)=>{
             state.status.message = "login successful";
             state.user = {...state.user, ...action.payload};
         })
-        .addCase(userLogin.rejected,(state, action)=>{
+        .addCase(loginUser.rejected,(state, action)=>{
             state.status.message = "login failure"
             state.error.message = JSON.stringify(action.error.message);
         })
