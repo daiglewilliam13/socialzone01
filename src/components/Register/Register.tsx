0aed80ad4fea8e7957e06639b1b746ceed827c59
William Daigle <daiglewilliam13@gmail.com>
2022-06-02 14:59:51 -0700

    add register component, add conditional render

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="register-wrapper">
+            <p>REGISTER COMPONENT</p>
+        </div>
+    )
+}
+
+export default Register;
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from '../../components/Login/Login';
+import Register from '../../components/Register/Register';
 import './landing.css';
 
 const Landing = () => {
-
+    const [showLogin, setShowLogin] = useState(true)
+    const handleClick = (e:any) =>{
+        setShowLogin(showLogin => !showLogin);
+    }
     return(
+        <>
         <div id="landing-wrapper">
-            <Login />
+            {(showLogin)
+            ? <Login />
+            : <Register />
+        }
+            <button onClick={(e)=>handleClick(e)}>Register</button>
         </div>
+        </>
     )
 }
 
