592fb44753a98ae8520c619670141cb4950c169e
William Daigle <daiglewilliam13@gmail.com>
2022-06-02 16:07:02 -0700

    add redirect logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Redirect } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {RootState } from './app/store';
 
 function App() {
+  const isLoggedIn = useSelector((state:RootState)=> state.authStatus.isLoggedIn)
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="home" element={<Home />} />
+        <Route path="home">
+        {isLoggedIn ? <Redirect to="/home" /> : <Home/>}
+        </Route>
       </Routes>
     </div>
   );
