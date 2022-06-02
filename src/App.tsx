2bc4408c782203ad4db18d3d73689fbd312765f4
William Daigle <daiglewilliam13@gmail.com>
2022-06-02 16:33:13 -0700

    protect routes wtih navigate

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import './App.css';
 import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
-import { Routes, Route, Redirect } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {RootState } from './app/store';
 
 function App() {
   const isLoggedIn = useSelector((state:RootState)=> state.authStatus.isLoggedIn)
+  console.log(isLoggedIn)
   return (
     <div className="App">
+      <>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="home">
-        {isLoggedIn ? <Redirect to="/home" /> : <Home/>}
-        </Route>
+        <Route path="/" element={<Landing />}/> 
+        <Route path="/home" element={
+        isLoggedIn ? <Home /> : <Navigate to="/" />
+        }/>
+
       </Routes>
+      </>
     </div>
   );
 }
