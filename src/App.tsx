872ce8736959763644b2d5709a4e730f3631d650
William Daigle <daiglewilliam13@gmail.com>
2022-06-06 11:17:40 -0700

    add messages and notifcations pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 import Friends from './pages/Friends/Friends';
+import Messages from './pages/Messages/Messages';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {RootState } from './app/store';
+import Notifications from './pages/Notifications/Notifications';
 
 function App() {
   const isLoggedIn = useSelector((state:RootState)=> state.authStatus.isLoggedIn)
@@ -21,6 +23,8 @@ function App() {
         }/>
           <Route path="/profile" element={<Profile />} />
           <Route path="/friends" element={<Friends />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/notifications" element={<Notifications />} />
       </Routes>
       </>
     </div>
