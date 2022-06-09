b9f12d95ccd816e24a38c917a15e104d94124455
William Daigle <daiglewilliam13@gmail.com>
2022-06-09 14:57:49 -0700

    update post types

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,69 +1,31 @@
-import React from "react";
-import {useState} from 'react';
+import React, { FC } from "react";
+import { useState } from 'react';
 import profilePicture from '../../images/jane-doe.jpg';
 import { BiDownArrow, BiUpArrow } from 'react-icons/bi';
 import './post.css';
 import PostComment from '../PostComment/PostComment';
 import Reply from '../Reply/Reply';
 
-const postObject = {
-    author: "Jane Doe",
-    date: "Sat, Jun 11 @ 11:35am",
-    body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere tempora culpa rem veniam a voluptatibus dolorem praesentium minima? Omnis nulla odio reprehenderit? Deserunt reiciendis fuga totam, atque molestiae omnis fugiat!",
-    comments: [{
-        authorId: 23,
-        date: "Sat, Jun 11th @ 11:34am",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    }, 
-    {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        }, {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        },
-        {
-            authorId: 23,
-            date: "Sat, Jun 11th @ 11:34am",
-            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-        }],
-    likes: 35,
-    likedBy: [12, 234, 23, 123],
-    dislikes: 3,
-    dislikedBy: [25, 2, 14]
+interface PostProps {
+    post: {
+        _id: string,
+        authorId: string,
+        authorName: string,
+        body: string,
+        comments: [string],
+        created: { type: Date },
+        likes: { type: Number },
+        dislikes: { type: Number },
+        likedBy: [string],
+        dislikedBy: [string],
+    }
 }
+const Post: FC<PostProps> = (props): JSX.Element => {
 
-const Post = () => {
+    const postObject = props.post
     const [expandComments, setExpandComments] = useState(false);
     const showComments = () => {
-        setExpandComments(expandComments=> !expandComments)
+        setExpandComments(expandComments => !expandComments)
     }
     let commentsClassStr = expandComments ? "expanded" : "collapsed";
     return (
@@ -72,8 +34,8 @@ const Post = () => {
                 <img alt-text="jane doe" src={profilePicture} />
             </div>
             <div className="content-heading-wrapper">
-                <h2>{postObject.author}</h2>
-                <p className="heading-date">on {postObject.date}</p>
+                <h2>{postObject.authorName}</h2>
+                <p className="heading-date">on {postObject.created}</p>
             </div>
             <div className="content">
 
@@ -87,16 +49,16 @@ const Post = () => {
                     <div className="vote-wrapper">
                         <BiUpArrow className="vote-icon" />{postObject.likes} : <BiDownArrow className="vote-icon" />{postObject.dislikes}
                     </div>
-                    <div className="comments-numbers">{postObject.comments.length} Comments<button onClick={showComments}className="show-comments">{expandComments? "hide" : "show"}</button></div>
+                    <div className="comments-numbers">{postObject.comments.length} Comments<button onClick={showComments} className="show-comments">{expandComments ? "hide" : "show"}</button></div>
                 </div>
             </div>
             <div className={`comments-wrapper ${commentsClassStr}`}>
-                {postObject.comments.map((comment) => {
+                {/* {postObject.comments.map((comment) => {
                     return (
                         <PostComment comment={comment} />
                     )
                 })
-                }
+                } */}
             </div>
         </div>
     )
