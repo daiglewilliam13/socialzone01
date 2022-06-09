4f082862c087368c2882295862093ac108f3856f
William Daigle <daiglewilliam13@gmail.com>
2022-06-09 14:28:48 -0700

    update show post timeline logic

--- src/components/ShowProfile/ShowProfile.tsx
+++ src/components/ShowProfile/ShowProfile.tsx
@@ -18,6 +18,13 @@ const initialState = {
     timelineComments: []
 
 }
+const initialPostState = [{
+    body: '',
+    comments: '',
+    authorId: '',
+    authorName: '',
+    createdAt: '',
+}]
 
 const getUser = async (id: string, token: string) => {
     const response = await fetch('http://localhost:8080/v1/users/' + id, {
@@ -31,36 +38,41 @@ const getUser = async (id: string, token: string) => {
     return (await response.json());
 }
 
-const getPosts = async (id: string) =>{
-    const response = await fetch('http://localhost:8080/v1/posts/author/'+ id,{
+const getPosts = async (id: string) => {
+    const response = await fetch('http://localhost:8080/v1/posts/author/' + id, {
         method: 'GET',
         mode: 'cors',
     })
     return (await response.json());
 }
 const ShowProfile: FC<ProfileProps> = (props): JSX.Element => {
-    const [userInfo, setUser] = useState(initialState)
+    const [userInfo, setUserInfo] = useState(initialState)
+    const [posts, setPosts] = useState([{body:''}])
     const token = useSelector((state: RootState) => state.authStatus.auth.tokens.access.token)
     useEffect(() => {
-        getPosts(props.id).then((res)=>console.log(res))
-        getUser(props.id, token).then((res) => { setUser(res) })
+        getPosts(props.id).then((res) => setPosts(res))
+        getUser(props.id, token).then((res) => { setUserInfo(res) })
     }, [props])
     return (
-        <div className="profile-wrapper">
-            <div className="profile-header">
-                <img className="profile-header-img" src={img}></img>
-                <p>{userInfo.name}</p>
-                <p>{userInfo.timelinePosts.length} posts</p>
-                <p>{userInfo.timelineComments.length} comments</p>
-                <p>{userInfo.isEmailVerified ? "Verified" : "Not Verified"} </p>
-                <button>Add to Friends</button>
-                <button>Message</button>
-                <button>Block</button>
+        <>
+            <div className="profile-wrapper">
+                <div className="profile-header">
+                    <img className="profile-header-img" src={img}></img>
+                    <p>{userInfo.name}</p>
+                    <p>{userInfo.timelinePosts.length} posts</p>
+                    <p>{userInfo.timelineComments.length} comments</p>
+                    <p>{userInfo.isEmailVerified ? "Verified" : "Not Verified"} </p>
+                    <button>Add to Friends</button>
+                    <button>Message</button>
+                    <button>Block</button>
+                </div>
+                <div className="timeline-wrapper">
+                    <h3>Posts by {userInfo.name}</h3><hr></hr>
+                </div>
+                <div>{posts[0].body}
+                </div>
             </div>
-            <div className="timeline-wrapper">
-                <h3>Posts by {userInfo.name}</h3><hr></hr>
-            </div>
-        </div>
+        </>
     )
 }
 
