781e5b32a3ce3da247b4501e809746516be39286
William Daigle <daiglewilliam13@gmail.com>
2022-06-15 15:32:03 -0700

    update prop types for createPost

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -2,14 +2,17 @@ import React, {FC} from 'react';
 import './createpost.css';
 
 interface CreateProps {
-    id: string
+    user: {
+        id: string,
+        name: string,
+    }
 }
 
 const CreatePost: FC<CreateProps> = (props): JSX.Element => {
     
     return(
         <div id="create-post-wrapper">
-            <p>What's on your mind?</p>
+            <p>What's on your mind, {props.user.name}?</p>
             <div className="create-post-input-wrapper">
                 <textarea name="" id="" placeholder='Tell us something about your day...'></textarea>
                 <button id="create-post-submit">Post Update</button>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import TopNav from '../../components/TopNav/TopNav';
 import SideNav from '../../components/SideNav/SideNav';
 import CreatePost from '../../components/CreatePost/CreatePost';
 import Feed from '../../components/Feed/Feed';
 import FriendList from '../../components/FriendList/FriendList';
 import './home.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
+
 
 const Home = () => {
- return (
-        <>
-        <TopNav />
-        <SideNav />
-        <div className="home-wrapper">
-        <CreatePost />
-        <Feed />
-        <FriendList />
-        </div>
-        </>
- )
+       const userInfo = useSelector((state:RootState)=>state.authStatus.auth.user)
+       const user = {
+              name: userInfo.name,
+              id: userInfo.id
+       }
+       return (
+              <>
+                     <TopNav />
+                     <SideNav />
+                     <div className="home-wrapper">
+                            <CreatePost user={user} />
+                            <Feed />
+                            <FriendList />
+                     </div>
+              </>
+       )
 }
 
 export default Home;
