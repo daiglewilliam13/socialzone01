177a40512c454db6ea4e532f57352790ab11be63
William Daigle <daiglewilliam13@gmail.com>
2022-06-15 15:38:01 -0700

    add logic to get post text from text area

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useState } from 'react';
 import './createpost.css';
 
 interface CreateProps {
@@ -9,13 +10,16 @@ interface CreateProps {
 }
 
 const CreatePost: FC<CreateProps> = (props): JSX.Element => {
-
+    const [postText, setPostText] = useState('');
+    const publishPost = async () =>{
+        console.log(postText)
+    }
     return (
         <div id="create-post-wrapper">
             <p>What's on your mind, {props.user.name}?</p>
             <div className="create-post-input-wrapper">
-                <textarea name="" id="" placeholder='Tell us something about your day...'></textarea>
-                <button id="create-post-submit">Post Update</button>
+                <textarea name="" id="" placeholder='Tell us something about your day...' onChange={(e)=>{setPostText(e.target.value)}}></textarea>
+                <button id="create-post-submit" onClick={publishPost}>Post Update</button>
             </div>
         </div>
     )
