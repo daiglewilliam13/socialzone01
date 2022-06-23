b6e7854f22ae844aca4b698fa97ca2dc7bcab5e3
William Daigle <daiglewilliam13@gmail.com>
2022-06-23 15:25:01 -0700

    update styles to allow sidebar space

--- src/pages/Following/Following.tsx
+++ src/pages/Following/Following.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
 import SideNav from "../../components/SideNav/SideNav";
 import TopNav from "../../components/TopNav/TopNav";
 
 
 const Friends = () => {
-
+    const allowSideBar = useSelector((state: RootState) => state.sideNavStatus.expanded)
+    let classStr = allowSideBar == 'true' ? 'allow-sidebar' : '';
     return (
         <>
             <TopNav />
             <SideNav />
-            <div className="content-wrapper">
+            <div className={`content-wrapper ${classStr}`}>
                 <p>CONTENT</p>
             </div>
         </>
