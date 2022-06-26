1165494864bf04bb1b83b3ff10a24b085d8561d2
William Daigle <daiglewilliam13@gmail.com>
2022-06-25 22:18:14 -0700

    built search result page

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -1,2 +1,14 @@
 import React from "react";
+import SideNav from "../../components/SideNav/SideNav";
+import TopNav from "../../components/TopNav/TopNav";
 
+const SearchResults = () =>{
+
+    return(
+        <>
+        <SideNav />
+        <TopNav />
+        <p>SEARCH RESULTS</p>
+        </>
+    )
+}
