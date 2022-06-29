660485ee51110d1e16b6702e7861671a75fdeee3
William Daigle <daiglewilliam13@gmail.com>
2022-06-29 14:15:09 -0700

    add populated db data for card content

--- src/components/NotificationCard/NotificationCard.tsx
+++ src/components/NotificationCard/NotificationCard.tsx
@@ -1,21 +1,26 @@
 import React, { FC, useState, useEffect } from "react";
-import './Reply.css';
 import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
 import { RootState } from '../../app/store';
 
-interface NotificationCardProps {
-    createdAt: string,
-    eventLink: string,
-    eventType: string,
-    read: boolean,
-    recipient: string,
-    sender: string,
-    id: string,
+interface NoticeProps {
+    notice:{
+        createdAt: string,
+        eventLink: string,
+        eventType: string,
+        read: boolean,
+        recipient: string,
+        sender: string,
+        id: string,
+    }
 }
-const NotificationCard: FC<NotificationCardProps> = props =>{
+const NotificationCard: FC<NoticeProps> = props =>{
 
     return(
-        <p>CARD COMPONENT</p>
+        <>
+            <p><Link to={props.notice.eventLink}>New {props.notice.eventType}!</Link></p>
+        <p></p>
+        </>
     )
 }
 
--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,12 +1,13 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import SideNav from "../../components/SideNav/SideNav";
 import TopNav from "../../components/TopNav/TopNav";
-
+import NotificationCard from "../../components/NotificationCard/NotificationCard";
 
 
 const Notifications = () => {
+    const [notifications, setNotifications] = useState([])
     const userInfo = useSelector((state:RootState)=>state.authStatus.auth.user)
     const getNotifications = async () =>{
         const url = "http://localhost:8080/v1/notifications/"+userInfo.id;
@@ -17,19 +18,25 @@ const Notifications = () => {
                 'Content-Type': 'application/json',
             }
         })
-        console.log(await response.json())
+       return response.json();
     }
     const allowSideBar = useSelector((state: RootState) => state.sideNavStatus.expanded)
     let classStr = allowSideBar == 'true' ? 'allow-sidebar' : '';
-    useEffect(()=>{
-        getNotifications();
+    const notificationsArray = notifications.map((notice)=>{
+        return(
+            <NotificationCard notice={notice} />
+        )
     })
+    useEffect(()=>{
+        getNotifications()
+        .then((data)=>setNotifications(data));
+    },[])
     return (
         <>
             <TopNav />
             <SideNav />
             <div className={`content-wrapper ${classStr}`}>
-                <p>NOTIFICATIONS CONTENT</p>
+                {notificationsArray}
             </div>
         </>
     )
